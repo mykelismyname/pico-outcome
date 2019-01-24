481fda294303c5ccf4bfb1696beb1aa27b1e20dd
michealabaho <michealabaho265@gmail.com>
2019-01-24 18:32:06 +0000

    extracting a few abstracts for the outcomes whose context was needed

--- annotate_text.py
+++ annotate_text.py
@@ -3,87 +3,122 @@ import pandas as pd
 
 from nltk.tokenize import word_tokenize
 import ebm_nlp_demo as e
-from pathlib import Path
 import re
-import string
-import operator
+from glob import glob
 
 class annotate_text:
 
     def __init__(self):
-        self.turker, self.ebm_extract = e.read_anns('hierarchical_labels', 'interventions', \
-                                      ann_type = 'aggregated', model_phase = 'test/gold')
+        self.turker, self.ebm_extract = e.read_anns('hierarchical_labels', 'outcomes', \
+                                      ann_type = 'aggregated', model_phase = 'train')
 
     def xml_wrapper(self):
 
         main_dir = 'adding_tags_to_ebm/'
-        data_dir = os.path.abspath(os.path.join(main_dir, 'aggregated', 'test_spans.txt'))
+        data_dir = os.path.abspath(os.path.join(main_dir, 'aggregated', 'train_spans.txt'))
         if not os.path.exists(os.path.dirname(data_dir)):
             os.makedirs(os.path.dirname(data_dir))
         print('Tagged spans are being written in file location %s'%(data_dir))
-        out_label, lab_outcome = {}, []
+        lab_outcome = []
         lab_outcome.clear()
+        context_file = open('lack_context.txt', 'w')
+
         with open(data_dir, 'w') as new_f:
             for pmid, doc in self.ebm_extract.items():
                 text = (doc.text).lower()
                 outcomes = list(set(e.print_labeled_spans_2(doc)))
                 outcomes_ordered_by_len = sorted(outcomes, key=lambda x: len(x[1]), reverse=True)
 
-                for elem in outcomes_ordered_by_len:
-                    phrase = re.sub('[.]$','', elem[1])
-                    text = text.replace(phrase, '<Outcome {}>{}</Outcome>'.format(elem[0], phrase.upper()))
+                #print('\n'+text+'\n',outcomes_ordered_by_len)
+                mental_outcomes = [i for i in list(set(outcomes_ordered_by_len)) if i[0] == 'Physical' and str(i[1]).__contains__('carcass performance')]
+                for elem in list(set(mental_outcomes)):
+                    # phrase = re.sub('[.]$','', elem[1])
+                    # #text = text.replace(phrase, '<Outcome {}>{}</Outcome>'.format(elem[0], phrase.upper()))
+                    #
+                    # lab_outcome.append(elem)
+                    # if str(elem[0]) == 'Mental':
+                    #     if(str(elem[1]).__contains__('number of picture exchanges in a far-transfer')):
+                    #         context_file.write(text)
+                    #     else:
+                    context_file.write(text+'\n')
+                    break
 
-                    lab_outcome.append(elem)
 
-                new_f.write('PMID - file %s\n'%(pmid)+text)
 
-        return set(lab_outcome)
+
+
+                #new_f.write('PMID - file %s\n'%(pmid)+text)
+            context_file.close()
+        return set(outcomes_ordered_by_len)
 
     def df_frame(self):
         discovered_outcomes =  list(self.xml_wrapper())
         discovered_outcomes_organised = sorted(discovered_outcomes, key=lambda l : l[0])
-        #discovered_outcomes_organised = [(i[0].strip(), i[0].strip().split)]
         cleaned_labels_outcomes = []
         for tup in discovered_outcomes_organised:
-            pattern_x = re.compile(r'[\[\(]\s[\w\s/._+-]+\s[\)\]]')
-            irregular_string = pattern_x.search(tup[1])
+            '''look for words inside circle or square braces within the e.g. "Lung injuries [ 'TPN' ]" contains a word TPN in square braces and if found, 
+            strip off the spaces and retain a cleaned outcome'''
+            pattern_x = re.compile(r'[\[\(]\s+[\w\s,;:%/._+-]+\s+[\)\]]')
+            irregular_string = pattern_x.findall(tup[1])
             if irregular_string:
-                regularised_string = ''.join([char for char in irregular_string.group().split()])
-                print(irregular_string.group(), '  ', regularised_string)
-        #         out_come = re.sub(str(irregular_string.group()), str(regularised_string).strip('()'), tup[1])
-        #     else:
-        #         out_come = tup[1]
-        #     cleaned_outcome = re.sub(r'[-_+.;:]$', '', out_come)
-        #     cleaned_labels_outcomes.append((tup[0], cleaned_outcome.strip()))
-        #
-        # cleaned_labels_outcomes_dict = dict(cleaned_labels_outcomes)
-        # df_frame = pd.DataFrame.from_dict(cleaned_labels_outcomes)
-        # print(df_frame)
-        # for i in cleaned_labels_outcomes:
-        #     print(i)
+                out_come = str(tup[1])
+                for item in irregular_string:
+                    regularised_string = item[0] + item[2:-2] + item[-1]
+                    out_come = out_come.replace(item, regularised_string)
+            else:
+                out_come = str(tup[1])
+            cleaned_outcome = re.sub(r'[-_+.,;:]$', '', out_come)
+            if cleaned_outcome:
+                cleaned_labels_outcomes.append((tup[0], cleaned_outcome.strip()))
+
+        cleaned_labels_outcomes_dict = dict(cleaned_labels_outcomes)
+        df_frame = pd.DataFrame.from_dict(cleaned_labels_outcomes)
+        df_frame.columns = ['Label','Outcome']
+
+        new_dir = os.path.abspath('adding_tags_to_ebm/aggregated')
+        if not os.path.exists(new_dir):
+            os.makedirs(new_dir)
+        df_frame.to_csv(os.path.join(new_dir, 'aggregated_test_crowd.csv'))
+
+#phase two of cleaning up, phrases where puncutation is hunging e.g. bone marrow , offset are turned to bone marrow, offset, and un-necessary punctuation at the start of sentences is eliminated
+def sub_span(outcome_string):
+    pattern_y = re.compile(r'(.\s[.,:;!?%])')
+    fix_string = pattern_y.findall(outcome_string)
+    if fix_string:
+        for j in fix_string:
+            outcome_string = outcome_string.replace(j, str(j[0]+j[2]))
+    else:
+        outcome_string = outcome_string
+    outcome_string = re.sub(r'^[\s.,;:!?\)\]]', '', outcome_string)
+    return outcome_string
+
+#concatenate all the frames extracted from the different annotations
+def final_label_outcome(ann_type=[]):
+    list_of_frames = []
+    for ann in ann_type:
+        locate_dir = os.path.abspath(os.path.join('adding_tags_to_ebm', ann))
+        csv_files = glob(os.path.join(locate_dir, '*.csv'))
+
+        for file in csv_files:
+            f = pd.read_csv(file)
+            f = f[['Label','Outcome']]
+            list_of_frames.append(f)
+
+    concat_frames = pd.concat(list_of_frames)
+    concat_frames_sorted = concat_frames.sort_values(by='Label')
+    print(concat_frames_sorted)
+
+    concat_frames_sorted.drop_duplicates(subset=['Label','Outcome'], keep=False)
+    concat_frames_sorted['Outcome'] = concat_frames_sorted['Outcome'].apply(lambda x: sub_span(x))
+    concat_frames_sorted = concat_frames_sorted.loc[concat_frames_sorted['Outcome'].str.len() > 1]
+    print(concat_frames_sorted.shape)
 
 
-        #     searched_text = ' '.join(word_tokenize(str(doc.text)))
-        #     lab_span = e.print_labeled_spans_2(doc)
-        #     #print(searched_text+'\n')
-        #     for tup in lab_span:
-        #         if(tup[1] not in string.punctuation):
-        #             #new_f.write('{} : {}'.format(str(tup[0]),str(tup[1]))+'\n')
-        #             try:
-        #                 searched_text = re.sub(tup[1], '<Outcome {}> {} </Outcome>'.format(tup[0], tup[1]), searched_text)
-        #             except Exception as d:
-        #                 #error_file.write('{} : {}'.format(tup[0],tup[1]) + '\n')
-        #                 print (d, tup[0], tup[1])
-        #     #annotate_f.write('{}'.format(pmid) + '\n' + '\n')
-        #     #annotate_f.write(searched_text + '\n' + '\n')
-        # #new_f.close()
-        # # annotate_f.close()
+    concat_frames_sorted.to_csv('labels_outcomes.csv')
 
 
 
-def sub_span(label, old_span, data):
-    new_span  = re.sub(old_span, '<Outcome {}>{}</outcome>'.format(label, old_span.upper()), data)
-    return new_span
+#final_label_outcome(['aggregated', 'individual'])
 
 run = annotate_text()
-run.df_frame()
+run.xml_wrapper()
